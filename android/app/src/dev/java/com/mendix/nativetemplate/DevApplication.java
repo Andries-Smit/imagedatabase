2ae92abb0ef8ef0b7be517ad1a1cebc514eea8e5
andries-smit <mr.andries.smit@gmail.com>
2020-10-23 15:34:15 +0200

    Inject flipper

--- android/app/src/dev/java/com/mendix/nativetemplate/DevApplication.java
+++ android/app/src/dev/java/com/mendix/nativetemplate/DevApplication.java
@@ -1,6 +1,14 @@
 package com.mendix.nativetemplate;
 
 import com.mendix.mendixnative.react.splash.MendixSplashScreenPresenter;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
 
 public class DevApplication extends MainApplication {
     @Override
@@ -11,5 +19,41 @@ public class DevApplication extends MainApplication {
     @Override
     public MendixSplashScreenPresenter createSplashScreenPresenter() {
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    }
+
+    /**
+     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+     *
+     * @param context
+     */
+    private static void initializeFlipper(
+            Context context, ReactInstanceManager reactInstanceManager) {
+        // if (BuildConfig.DEBUG) {
+            try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+                Class<?> aClass = Class.forName("com.mendix.nativetemplate.ReactNativeFlipper");
+                aClass
+                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                        .invoke(null, context, reactInstanceManager);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        // }
     }
 }
